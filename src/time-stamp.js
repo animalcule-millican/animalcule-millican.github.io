a64a6f201389a68d763fceea20842c8c41ace181
animalcule-millican <millican.m@gmail.com>
2023-09-25 13:32:02 -0500

    updates

--- src/time-stamp.js
+++ src/time-stamp.js
@@ -1,14 +1,8 @@
-// Get the current point in time 
-const timestamp = new Date();
-
-// advance by 24 hours, to get to tomorrow
-timestamp.setTime(timestamp.getTime() + (24 * 60 * 60 * 1000));
-
-// format and output the result
-const locale = undefined; // use the user's current locale (language/region)
-document.getElementById("spanDate").innerHTML = timestamp.toLocaleString(locale, {
+var timestamp = new Date();
+var spanDate = document.getElementById("spanDate").innerHTML = timestamp.toLocaleString({
   timeZone: 'America/Chicago', // use the US Central Time Zone
   year: 'numeric', // include the year as a number
   month: 'long',   // include the month as the long name
   day: 'numeric'   // include the day as a number
 });
+spanDate.innerHTML = currentDate.toLocaleString();
